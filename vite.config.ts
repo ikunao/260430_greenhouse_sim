8aba706a393caa2b9ff9119b5dcc1423fee23d9f
ikunao <147695559+ikunao@users.noreply.github.com>
2026-04-30 12:55:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/260430_greenhouse_sim/'
+    base: '/260430_greenhouse_sim/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
